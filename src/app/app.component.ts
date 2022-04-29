a57c2e179efd9cdacc9a9c3f002dc4f0195a6c69
Mohamed Emad <memad352@gmail.com>
2022-04-29 19:46:41 +0200

    Fixed control menu bug with tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('overlay') overlay?: ElementRef;
 
   constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    // Disabling tab because it's causing a bug with the control menu
+    document.onkeydown = (e: KeyboardEvent) => {
+      if (e.key === 'Tab') e.preventDefault();
+    };
+  }
 
   isOverlayed = false;
 
